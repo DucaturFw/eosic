a0b6ace256da60715ed0cf0a235105228994a01f
Aler Denisov <aler.zampillo@gmail.com>
2018-08-04 16:10:26 +0300

    make docker wrapper more generic

---
+++ src/lib/eos-docker.ts
@@ -0,0 +1,44 @@
+import * as path from "path";
+import Docker, { IDockerOptions } from "./docker";
+import { DeepPartial } from "../utils";
+
+export const binariesPath = path.resolve(__dirname, "..", "..", "bin");
+
+export default class EosDocker extends Docker {
+  defaultOptions(): DeepPartial<IDockerOptions> {
+    return {
+      cwd: process.cwd(),
+      image: {
+        repository: "eosio/eos-dev",
+        tag: "latest"
+      },
+      container: {
+        name: `eosic-${(Math.random() * 0xffffff) >> 0}`,
+        binds: {
+          "/opt/eosio/bin/data-dir/config.ini": () =>
+            `${this.options.cwd}/config.ini`,
+          "/contracts": () => `${this.options.cwd}/contracts`,
+          "/.bashrc": binariesPath + "/.bashrc",
+          "/eosiocppfix": binariesPath + "/eosiocppfix",
+          "/compile": binariesPath + "/compile"
+        },
+        command: [
+          "/opt/eosio/bin/nodeosd.sh --data-dir /opt/eosio/bin/data-dir -e --contracts-console"
+        ],
+        expose: [8888, 9876],
+        hostname: "test",
+        labels: {
+          eosic: "true",
+          "eosic.version": "0.0.1"
+        },
+        ports: {
+          "8888": 8888,
+          "9876": 9876
+        },
+        removeVolumes: true
+      },
+      stdout: true,
+      onStart: ["chmod +x /compile", "chmod +x /eosiocppfix"]
+    };
+  }
+}
